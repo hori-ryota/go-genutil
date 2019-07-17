87bb1998e31af3ef723c57b6aa4d4fba482b94be
hori-ryota <hori.ryota@gmail.com>
2019-07-17 19:59:37 +0900

    fix FieldName Parser

--- genutil/go_ast_field_name.go
+++ genutil/go_ast_field_name.go
@@ -1,11 +1,25 @@
 package genutil
 
-import "go/ast"
+import (
+	"fmt"
+	"go/ast"
+	"reflect"
+)
 
 func ParseFieldName(field *ast.Field) string {
 	if field.Names == nil {
 		// embedding
-		return field.Type.(*ast.Ident).Name
+		switch t := field.Type.(type) {
+		case *ast.Ident:
+			return t.Name
+		case *ast.SelectorExpr:
+			return t.Sel.Name
+		default:
+			panic(fmt.Errorf(
+				"failed to parse FieldType: unknown %s",
+				reflect.TypeOf(t),
+			))
+		}
 	}
 
 	return field.Names[0].Name
